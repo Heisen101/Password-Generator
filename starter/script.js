ed914cd62a0c3f9a5557d9c3f9d85c372cf5c7a3
Vasile <vasileholding@gmail.com>
2023-07-22 13:08:37 +0100

    finanaly fixed the bug, added prom parameter for the generatePassword function in password function

--- starter/script.js
+++ starter/script.js
@@ -155,7 +155,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(prom);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
